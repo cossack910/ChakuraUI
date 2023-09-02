ee8d99b9cacd9e675c5bf40bc6d53496a0c656a8
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-09-02 21:31:21 +0900

    一覧取得

--- app/src/components/organisms/user/UserCard.tsx
+++ app/src/components/organisms/user/UserCard.tsx
@@ -1,5 +1,5 @@
 import { FC, memo } from "react";
-import { Wrap, WrapItem, Box, Stack, Image, Text } from "@chakra-ui/react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
 
 type Props = {
   imageUrl: string;
@@ -10,35 +10,31 @@ type Props = {
 export const UserCard: FC<Props> = memo((props) => {
   const { userName, fullName, imageUrl } = props;
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <Box
-          w="260"
-          h="260"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              boxSize="160"
-              borderRadius="full"
-              src={imageUrl}
-              alt="プロフィール画像"
-              m="auto"
-            ></Image>
-            <Text fontSize="lg" fontWeight="bold">
-              {userName}
-            </Text>
-            <Text fontSize="sm" color="glay">
-              {fullName}
-            </Text>
-          </Stack>
-        </Box>
-      </WrapItem>
-    </Wrap>
+    <Box
+      w="260"
+      h="260"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          boxSize="160"
+          borderRadius="full"
+          src={imageUrl}
+          alt="プロフィール画像"
+          m="auto"
+        ></Image>
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="glay">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
   );
 });
 
--- app/src/components/pages/UserManagement.tsx
+++ app/src/components/pages/UserManagement.tsx
@@ -1,11 +1,33 @@
-import { memo, FC } from "react";
+import { memo, FC, useEffect } from "react";
+import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
+
 import { UserCard } from "../organisms/user/UserCard";
+import { UseAllUsers } from "../../hooks/useAllUsers";
+
 export const UserManagement: FC = memo(() => {
+  const { getUsers, users, loading } = UseAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <UserCard
-      userName="神田まさし"
-      fullName="kanda masashi"
-      imageUrl="https://source.unsplash.com/random"
-    />
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users?.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                userName={user.name}
+                fullName={user.username}
+                imageUrl="https://source.unsplash.com/random"
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
